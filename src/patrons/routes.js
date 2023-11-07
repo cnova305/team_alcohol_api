ac8f614899ccdccfc7b27f5ab88cfe7f6111a53a
Christopher Juwao <njuwao@icloud.com>
2023-11-07 17:41:39 +0200

    add update query

--- src/patrons/routes.js
+++ src/patrons/routes.js
@@ -7,6 +7,7 @@ const router = Router();
 router.get("/", controller.getPatrons);
 router.post("/", controller.addPatron);
 router.get("/:id", controller.getPatronById);
+router.put("/:id", controller.updatePatron);
 router.delete("/:id", controller.deletePatron);
 
 module.exports = router;
