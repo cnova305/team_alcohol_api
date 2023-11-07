6d1e8ba27d2098a4ecc3020d0156868e921a9071
Christopher Juwao <njuwao@icloud.com>
2023-11-07 17:42:06 +0200

    add update route

--- src/patrons/queries.js
+++ src/patrons/queries.js
@@ -1,6 +1,7 @@
 const getPatrons = "SELECT * FROM patrons";
 const getPatronById = "SELECT * FROM patrons WHERE id = $1";
 const addPatron = "INSERT INTO patrons (name, weight, id) VALUES ($1, $2, $3)";
+const updatePatron = "UPDATE patrons SET name = $1 WHERE id = $2";
 const deletePatron = "DELETE FROM patrons WHERE id = $1";
 
 module.exports = {
@@ -8,4 +9,5 @@ module.exports = {
   getPatronById,
   addPatron,
   deletePatron,
+  updatePatron,
 };
