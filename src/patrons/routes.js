f7225702d669ce26747f655618f6dcdf1ba0b160
Christopher Juwao <njuwao@icloud.com>
2023-11-08 14:41:58 +0200

    update delete route

--- src/patrons/routes.js
+++ src/patrons/routes.js
@@ -8,6 +8,6 @@ router.get("/", controller.getPatrons);
 router.post("/addPatron", controller.addPatron);
 router.get("/getPatronById/:id", controller.getPatronById);
 router.put("/:id", controller.updatePatron);
-router.delete("/:id", controller.deletePatron);
+router.delete("/deletePatronById/:id", controller.deletePatron);
 
 module.exports = router;
